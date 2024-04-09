2310861b49a70b6159005698e99e9b1df163df74
Breno1221 <123486496+Breno121zs@users.noreply.github.com>
2024-04-09 10:30:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+//ways to declare a variable
+//var, let, const
+
+let firstName = 'John';
+let lastName = 'Doe';
+
+console.log (firstName, lastName);
+
+let age = 30;
+
+console.log (age);
+
+//only letters, numbers, underscores and dollar signs
+//can´t star with 
+
+//multi-word formatting
+//firstName = camelCase
+//first_name = underscore
+//firstName = PascalCase
+//firstname = lowercase
+
+//Re-assigning Variables
+age = 31;
+
+console.log(age);
+
+let score;
+
+score = 1;
+
+console.log (score);
+
+if (true) {
+    score = score +1;
+}
+
+console.log (score);
+
+const x  = 100;
+
+const arr = [1, 2, 3, 4];
+
+arr.push (5);
+
+console.log (arr)
+
+const person = {
+    name: 'brad'
+};
+
+person.name = 'John'
+
+person.email = 'brad@gmail.com'
+
+console.log (person);
+
+let a, b, c
+
+const d = 10, e = 20, f = 30;
+
+console.log (d);
